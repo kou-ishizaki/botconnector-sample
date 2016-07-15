7256d1d4cfda871786e5d565d4229484ffe9d868
14H-02390 <kou06ishi16@gmail.com>
2016-07-15 23:08:49 +0900

    再修正

--- server.js
+++ server.js
@@ -5,17 +5,19 @@ var port = process.env.PORT || 8080;
  
 // Create bot and add dialogs
 var bot = new builder.BotConnectorBot({ appId: 'ffdf8cfa-5648-4055-bf00-955852fcc088', appSecret: '50saqFgDmP7yR1a7sL9E0Vs' });
-bot.add('/', function (session) {
 
-public class HelloWorld {
-public static void main(String[] args){
-System.out.println("Hello world.");
-}
-}
+var dialog = new builder.CommandDialog();
+
+dialog.matches(['Hi', 'Hello', 'こんにちは'], function (session) {
+session.send('こんにちは');
+});
+bot.add('/', dialog);
+
+//bot.add('/', function (session) {
 
 //   session.send('Hello World'); 
 
-});
+//});
  
 // Setup Restify Server
 var server = restify.createServer();
