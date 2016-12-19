2be6de3a6fb189c2a9ea921138862309dec112a5
14H-02390 <kou06ishi16@gmail.com>
2016-12-19 23:23:05 +0900

    ?よくわからん

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var dialog = new builder.CommandDialog();
 
 // 認識に指定するLUIS APIのURLを指定
 var recognizer = new builder.LuisRecognizer('https://api.projectoxford.ai/luis/v2.0/apps/ed81de53-5293-4f9e-acbb-41f678f4633a?subscription-key=3111f6e1c29d4036b49841e765412611&verbose=true
-');
+'');
 
 // IntentDialogオブジェクトを作成
 var intents = new builder.IntentDialog({
@@ -35,6 +35,7 @@ intents
         // インテントが 'intentA' だったときの処理をここに記述します。
 
     })
+intents
     .matches('intentB', function (session, args) {
 
         // インテントが 'intentB' だったときの処理をここに記述します。
